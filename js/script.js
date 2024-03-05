d994863edb8c984ae7808127321db8514269722f
simoneaucello <simoneaucello@gmail.com>
2024-03-05 15:10:38 +0100

    [add] array w/ info team

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const output = document.querySelector('.output')
+
+
+const team = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    img: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    img: 'img/angela-caroll-chief-editor.jpg'
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    img: 'img/walter-gordon-office-manager.jpg'
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    img: 'img/angela-lopez-social-media-manager.jpg'
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    img: 'img/scott-estrada-developer.jpg'
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    img: 'img/barbara-ramos-graphic-designer.jpg'
+  },
+]
